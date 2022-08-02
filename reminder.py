8c1f6e499164ace912d297f12687a4bb41de0c03
DickinsonBP <dickinsonbp@gmail.com>
2022-08-02 15:12:15 +0200

    update

--- reminder.py
+++ reminder.py
@@ -3,11 +3,10 @@ from requests.structures import CaseInsensitiveDict
 
 
 
-
 url = "https://graph.facebook.com/v13.0/103347919147898/messages"
 
 headers = CaseInsensitiveDict()
-headers["Authorization"] = "Bearer EAAHDZC1VSkZB0BALvKqzR7H7Vibgda5rY1uixAbuTjoAJrlZCOMCq55u7Kw2ZATBRgRVdWSXmlA1wMvsjnxxmS6wE5YgFxUtTzMDDITGSKEb11ZCWazyIviQGhs3eRyYCIZCCJxsUWJD56iDGzbmoda2gNWRK1OX8hoZBudaonJc53NXv5kMuMI6iJ9W1li0Aw9JU822W9PcgZDZD"
+headers["Authorization"] = "Bearer TOKEN"
 headers["Content-Type"] = "application/json"
 
 for number, name in numbers.items():
